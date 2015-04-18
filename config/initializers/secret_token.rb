904467c9c9b02c4f6307159523ac28e5c20e79fd
wildpetals <lauralee@wildpetals.net>
2015-04-17 21:36:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ufirstrails::Application.config.secret_key_base = '5622daf9a43ceca11ccebcf05b32d2ee5b57027d2d26613af41d22dc1199b7ef3f17a634b01d7b590ee5a4242b732f3e90c3430424638c1384d19c0ea01c9525'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ufirstrails::Application.config.session_store :cookie_store, key: '_ufirstrails_session'
